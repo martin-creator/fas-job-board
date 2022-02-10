0ba876f7ecc8be4e37f885b25ca33bc8bd996ca9
Ben Greenberg <bengreenberg@gmail.com>
2022-02-10 08:45:28 +0200

    update link check validator

--- app/validators/no_links_validator.rb
+++ app/validators/no_links_validator.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class NoLinksValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value && value.downcase.split(/(?=[.])/).any? { |x| [".com", ".org", ".dev", ".co", ".net", ".ly"].include?(x).present? }
+    if value && %r{(https?://)?\w*\.\w+(\.\w+)*(/\w+)*(\.\w*)?}.match(value).to_s.present?
       record.errors.add(attribute, "profile bio cannot contain any links")
     end
   end
