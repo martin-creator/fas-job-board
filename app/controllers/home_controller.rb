a059fd3c5db0c8559e815eacd36caa67610a5df0
Ben Greenberg <bengreenberg@gmail.com>
2022-02-20 17:42:06 +0200

    remove unused eager loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def show
     @developers = Developer
-      .includes(:role_type).with_attached_avatar
+      .with_attached_avatar
       .available.newest_first
       .limit(10)
   end
